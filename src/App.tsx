4d82ab6fb7fe7162505be28cf16c4c0db3860103
John Cook-Martin <johncookmartin@gmail.com>
2026-02-23 18:24:30 -0500

    removing unused React reference in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
