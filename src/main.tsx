ce848466fca1a130c0855e309ced5979ebf17687
John Cook-Martin <johncookmartin@gmail.com>
2026-02-23 17:57:09 -0500

    adding mui and logos

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import App from './App.tsx';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store.ts';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 const root = createRoot(document.getElementById('root')!);
 
 root.render(
