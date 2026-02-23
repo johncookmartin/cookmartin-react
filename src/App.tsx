d7afcebe07ec523d9f3ff019b76a63f6db5615ee
John Cook-Martin <johncookmartin@gmail.com>
2026-02-23 18:51:34 -0500

    changing font for Jacquie

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    fontFamily: '"Montserrat", "Helvetica", "Arial", sans-serif',
   },
 });
 
--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,10 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import '@fontsource/montserrat/300.css';
+import '@fontsource/montserrat/400.css';
+import '@fontsource/montserrat/500.css';
+import '@fontsource/montserrat/700.css';
 
 const root = createRoot(document.getElementById('root')!);
 
